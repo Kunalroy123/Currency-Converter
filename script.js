c68d310f6107fa2178f7daef8ae8bb45b1e93cf8
Kunal Roy <kunalroy9987@gmail.com>
2024-08-31 12:30:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("#btn");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg")
+
+
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click" , async (evt) =>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if (amtValue === "" || amtValue <1) {
+        amtValue = 1;
+        amount.value = "1";
+    }
+
+   
+    const fromCurrency = fromCurr.value.toLowerCase();
+    const toCurrency = toCurr.value.toLowerCase();
+
+    const URL = `${BASE_URL}/${fromCurrency}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurrency][toCurrency];
+    let final_rate = msg.innerText = `${amount.value} ${fromCurrency.toUpperCase()} = ${rate} ${toCurrency.toUpperCase()}`
+    console.log(final_rate)
+
+
+})
